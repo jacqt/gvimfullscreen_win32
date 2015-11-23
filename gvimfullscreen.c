f5b6a73048e435306f7161b67d5628d41256d9cf
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-11-23 21:42:00 +0000

    Fixed for windows 10

--- gvimfullscreen.c
+++ gvimfullscreen.c
@@ -45,45 +45,44 @@ LONG _declspec(dllexport) ToggleFullScreen()
 			GetWindowRect(hTop, &rc);
 			hMonitor = MonitorFromRect(&rc, MONITOR_DEFAULTTONEAREST);
 
-			// 
-			// get the work area or entire monitor rect. 
-			// 
+			//
+			// get the work area or entire monitor rect.
+			//
 			mi.cbSize = sizeof(mi);
 			GetMonitorInfo(hMonitor, &mi);
 
 			g_x = mi.rcMonitor.left;
 			g_y = mi.rcMonitor.top;
 			g_dx = mi.rcMonitor.right - g_x;
-			g_dy = mi.rcMonitor.bottom - g_y;
-			//cx = GetSystemMetrics(SM_CXSCREEN);
-			//cy = GetSystemMetrics(SM_CYSCREEN);
+			g_dy = mi.rcMonitor.bottom; // + 25 if you want complete full screen experience.
+                                  // but my computer has an artifact on the lower left corner so I don't want to see it
 
 			/* Remove border, caption, and edges */
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_EXSTYLE) & ~WS_BORDER); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) & ~WS_CAPTION); 
-			SetWindowLong(hTop, GWL_EXSTYLE, GetWindowLong(hTop, GWL_STYLE) & ~WS_EX_CLIENTEDGE); 
-			SetWindowLong(hTop, GWL_EXSTYLE, GetWindowLong(hTop, GWL_STYLE) & ~WS_EX_WINDOWEDGE); 
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_EXSTYLE) & ~WS_BORDER);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) & ~WS_CAPTION);
+			SetWindowLong(hTop, GWL_EXSTYLE, GetWindowLong(hTop, GWL_STYLE) & ~WS_EX_CLIENTEDGE);
+			SetWindowLong(hTop, GWL_EXSTYLE, GetWindowLong(hTop, GWL_STYLE) & ~WS_EX_WINDOWEDGE);
 
 			SetWindowPos(hTop, HWND_TOP, g_x, g_y, g_dx, g_dy, SWP_SHOWWINDOW);
 
-			/* Now need to find the child text area window 
-			 * and set it's size accordingly 
+			/* Now need to find the child text area window
+			 * and set it's size accordingly
 			 */
 			EnumChildWindows(hTop, EnumChildProc, 0);
 		}
 		else
 		{
 			/* Already full screen, so restore all the previous styles */
-			SetWindowLong(hTop, GWL_EXSTYLE, GetWindowLong(hTop, GWL_EXSTYLE) | WS_BORDER); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_CAPTION); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_SYSMENU); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_MINIMIZEBOX); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_MAXIMIZEBOX); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_SYSMENU); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_EX_CLIENTEDGE); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_EX_WINDOWEDGE); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_THICKFRAME); 
-			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_DLGFRAME); 
+			SetWindowLong(hTop, GWL_EXSTYLE, GetWindowLong(hTop, GWL_EXSTYLE) | WS_BORDER);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_CAPTION);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_SYSMENU);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_MINIMIZEBOX);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_MAXIMIZEBOX);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_SYSMENU);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_EX_CLIENTEDGE);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_EX_WINDOWEDGE);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_THICKFRAME);
+			SetWindowLong(hTop, GWL_STYLE, GetWindowLong(hTop, GWL_STYLE) | WS_DLGFRAME);
 
 
 			SendMessage(hTop, WM_SYSCOMMAND, SC_RESTORE, 0);
@@ -97,15 +96,15 @@ BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lParam)
 {
 	char lpszClassName[100];
 	GetClassName(hwnd, lpszClassName, 100);
-	if ( strcmp(lpszClassName, "VimTextArea") == 0 ) 
+	if ( strcmp(lpszClassName, "VimTextArea") == 0 )
 	{
 		//int cx, cy;
 		//cx = GetSystemMetrics(SM_CXSCREEN);
 		//cy = GetSystemMetrics(SM_CYSCREEN);
 
-		SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_EX_CLIENTEDGE); 
-		SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_EX_WINDOWEDGE); 
-		SetWindowPos(hwnd, HWND_TOP, 0, 0, g_dx, g_dy, SWP_SHOWWINDOW);
+		SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_EX_CLIENTEDGE);
+		SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_EX_WINDOWEDGE);
+		SetWindowPos(hwnd, HWND_TOP, 0, 38, g_dx, g_dy, SWP_SHOWWINDOW);
 
         SetClassLong(hwnd, GCL_HBRBACKGROUND, CreateSolidBrush(RGB(0,0,0)));
 	}
